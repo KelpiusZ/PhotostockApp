f2e93769b700ae1d0d8f0973071266dec1b92747
kel <kelpius21@gmail.com>
2021-01-15 15:05:25 -0500

    Added functionality to the search bar, and the search button. Also now mapping through the photos array returned from the API and rendering it in the Main component and onto the screen.

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,29 +2,37 @@ import React from 'react';
 
 
 const Main = ({ images }) => {
+    // let myphotos = images.photos
+    
+    // console.log(myphotos)
 
-    // console.log(images)
-
-    if(!images.length){
-        { console.log(images.url)}
-        return <h2>Still No Images Found! bruhhhhh</h2>
-    }
-
-    // console.log(images)
+    // if(!images.length){
+    //     { console.log(images.url)}
+    //     return <h2>Still No Images Found! bruhhhhh</h2>
+    // }
+    
     return (
-
+        
         <div className="main">
 
             <h3>This is the main part of the page</h3>
-            {images.map(image =>(
-                <div key={image.id}>
-                    <img src={image.images.url}/>
+            {images.photos.map(image =>{
+                
+                return (
+                    
+                    <div key={image.id}>
+                    <img src={image.src.medium}/>
                 </div>
-            ))}            
+
+)
+
+})}            
             
         </div>
 
 );
+
+
 
 }
 
